e5317b36bb3e3696d379e685565acca1cfbc691f
Diego <dfreile51@gmail.com>
2021-11-16 09:55:30 +0100

    02 Añadido imprimeAutor y imprimeTitulo

--- Libro.java
+++ Libro.java
@@ -29,4 +29,12 @@ public class Libro {
     public String getTitulo() {
         return titulo;
     }
+    
+    public void imprimeAutor() {
+        System.out.println(autor);
+    }
+    
+    public void imprimeTitulo() {
+        System.out.println(titulo);
+    }
 }
